25f2aaf722617a98f059a241ea995a68ffe558d3
Jake Quinter <hello@jakequinter.io>
2022-04-02 18:44:23 -0400

    heart icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
+import { FiHeart } from 'react-icons/fi';
 
 import Cards from 'src/components/Cards';
 import Nav from 'src/components/layout/Nav/Nav';
@@ -14,8 +15,9 @@ const Home: NextPage = () => {
             <h1 className="mb-4 text-6xl font-semibold leading-tight text-slate-900">
               Memorable pieces you are guranteed to love
             </h1>
-            <h2 className="flex items-center justify-center text-2xl">
-              Memories are timeless treasures of the heart ❤️
+            <h2 className="flex items-center justify-center text-2xl text-slate-600">
+              Memories are timeless treasures of the heart{' '}
+              <FiHeart className="ml-2 fill-[#f83431] text-[#f83431]" />
             </h2>
           </div>
 
